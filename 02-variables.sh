92898934e30a4ddb44ad2676666677dc5362d5ce
janidevops <konajanakirao@gmail.com>
2026-09-16 00:09:08 +0530

    shell-script

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-echo "Suresh : Hello Ramesh How are you?"
-echo "Ramesh : I am fine Suresh, How are you?"
-echo "Suresh : I am also fine Ramesh, What are you doing?"
-echo "Ramesh : I am learning shell scripting, What about you?"
-echo "Suresh : I am also learning shell scripting, What are you learning?"
+PERSON1="Suresh"  #there is no space between variable name and value
+PERSON2="Ramesh"
+
+echo "$PERSON1 : Hello $PERSON2 How are you?"
+echo "$PERSON2 : I am fine $PERSON1, How are you?"
+echo "$PERSON1 : I am also fine $PERSON2, What are you doing?"
+echo "$PERSON2 : I am learning shell scripting, What about you?"
+echo "$PERSON1 : I am also learning shell scripting, What are you learning?"
