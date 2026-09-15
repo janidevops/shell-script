5d1a5afc441a36d501158e250c1722caf5670d6c
janidevops <konajanakirao@gmail.com>
2026-09-16 00:14:41 +0530

    shell-script

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1  #$1 ia here arugument passed to the script  
+PERSON2=$2
+
+echo "$PERSON1 : Hello $PERSON2 How are you?"
+echo "$PERSON2 : I am fine $PERSON1, How are you?"
+echo "$PERSON1 : I am also fine $PERSON2, What are you doing?"
+echo "$PERSON2 : I am learning shell scripting, What about you?"
+echo "$PERSON1 : I am also learning shell scripting, What are you learning?"
