761ff68440f4d7f8fc5eaff9930fc56308a6dfd7
janidevops <konajanakirao@gmail.com>
2026-09-15 23:54:52 +0530

    shell-script

---
+++ 02-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Suresh : Hello Ramesh How are you?"
+echo "Ramesh : I am fine Suresh, How are you?"
+echo "Suresh : I am also fine Ramesh, What are you doing?"
+echo "Ramesh : I am learning shell scripting, What about you?"
+echo "Suresh : I am also learning shell scripting, What are you learning?"
