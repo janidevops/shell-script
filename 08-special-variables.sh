9bac56de5c4ca44f7814ece8933ae5f5c1ca42a0
janidevops <konajanakirao@gmail.com>
2026-09-16 11:39:45 +0530

    shell-script

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# interview question: What are special variables in shell scripting? Give examples ?.
 
 echo "All arguments/variables passed to the script: $@"
 echo "Number of arguments/variables passed to the script: $#"
@@ -7,3 +8,5 @@ echo "current working directory: $PWD"
 echo "The process ID of the current script: $$"
 echo "Home directory of the current user: $HOME"
 echo "which user is executing the script: $USER"
+echo "The exit status of the last command executed: $?"
+echo "Hostname of the machine: $HOSTNAMEg"
