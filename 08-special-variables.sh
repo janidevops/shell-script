e09a76071994fed4508e68e8186e64075d4a67d7
janidevops <konajanakirao@gmail.com>
2026-09-16 11:58:36 +0530

    shell-script

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -11,4 +11,3 @@ echo "which user is executing the script: $USER"
 echo "The exit status of the last command executed: $?"
 echo "Hostname of the machine: $HOSTNAME"
 echo "Process id of the last background command: $!"
-sleep 60
