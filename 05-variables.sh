8d8c7fb2d2b8bee2d5f2ac5b4b015d4333114e70
janidevops <konajanakirao@gmail.com>
2026-09-16 08:40:07 +0530

    shell-script

---
+++ 05-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Please Enter your username:: "
+read USERNAME
+echo "Please Enter your password:: "
+read  PASSWORD
+
+echo "Your username is $USERNAME and your password is $PASSWORD"
