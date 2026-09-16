2cf6d483e6b7d41ad4f64b00d48a08d9bdf856b0
janidevops <konajanakirao@gmail.com>
2026-09-16 11:52:24 +0530

    shell-script

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -10,3 +10,5 @@ echo "Home directory of the current user: $HOME"
 echo "which user is executing the script: $USER"
 echo "The exit status of the last command executed: $?"
 echo "Hostname of the machine: $HOSTNAME"
+
+sleep 60
