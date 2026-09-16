46300842049216e1f3c8a30e2114b708e4dfb3cf
janidevops <konajanakirao@gmail.com>
2026-09-16 08:42:55 +0530

    shell-script

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo "Please Enter your username:: "
-read USERNAME
-echo "Please Enter your password:: "
-read  PASSWORD
+read USERNAME # read command is used to take input from the user and store it in the variable USERNAME in terminal
+echo "Please Enter your password:: " 
+read -s PASSWORD  # read -s is used to hide the password input from the terminal
+echo
 
 echo "Your username is $USERNAME and your password is $PASSWORD"
