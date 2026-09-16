ebe2e78b6097a8466323bcec1cbd1a8be9b67646
janidevops <konajanakirao@gmail.com>
2026-09-16 11:35:41 +0530

    shell-script

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -3,3 +3,7 @@
 echo "All arguments/variables passed to the script: $@"
 echo "Number of arguments/variables passed to the script: $#"
 echo "The name of the script: $0"
+echo "current working directory: $PWD"
+echo "The process ID of the current script: $$"
+echo "Home directory of the current user: $HOME"
+echo "which user is executing the script: $USER"
