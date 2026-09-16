147d9266c8ed6cb260e37da69c9d1a6d94682a0c
janidevops <konajanakirao@gmail.com>
2026-09-16 08:51:15 +0530

    shell-script

--- 04-variables.sh
+++ 04-variables.sh
@@ -4,7 +4,7 @@ PERSON1=$1  #$1 ia here arugument passed to the script
 PERSON2=$2
 
 echo "$PERSON1 : Hello $PERSON2 How are you?"
-echo "$PERSON2 : I am fine $PERSON1, How are you?"
+echo "${PERSON2} : I am fine $PERSON1, How are you?"
 echo "$PERSON1 : I am also fine $PERSON2, What are you doing?"
 echo "$PERSON2 : I am learning shell scripting, What about you?"
 echo "$PERSON1 : I am also learning shell scripting, What are you learning?"
---
+++ 06-array.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+MOVIES=("Bahubali" "RRR" "KGF" "Vikram")
+
+echo "The first movie is ${MOVIES[0]}"
