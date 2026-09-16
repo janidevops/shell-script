217f96afe0a4870da78c57092f18f0051b53b51e
janidevops <konajanakirao@gmail.com>
2026-09-16 20:46:02 +0530

    shell-script

--- 10-installation.sh
+++ 10-installation.sh
@@ -5,6 +5,7 @@ USER_ID=$(id -u)
 if ([ $USER_ID -ne 0 ])
  then
     echo "Please run this script as the root user"
+    exit 1 # manually exit the script with a non-zero exit code
 else 
     echo "you are super user"
 fi
