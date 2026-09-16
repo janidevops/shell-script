79e6599fe8e3f9b7e01a3e61b0bd7646ad7f7db6
janidevops <konajanakirao@gmail.com>
2026-09-16 08:56:36 +0530

    shell-script

--- 06-array.sh
+++ 06-array.sh
@@ -3,3 +3,6 @@
 MOVIES=("Bahubali" "RRR" "KGF" "Vikram")
 
 echo "The first movie is: ${MOVIES[0]}"
+echo "The second movie is: ${MOVIES[1]}"
+echo "The third movie is: ${MOVIES[2]}"
+echo "The fourth movie is: ${MOVIES[3]}"
