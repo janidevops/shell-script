d398d3553f4e5735a542e501aac3be0590d4e4fc
janidevops <konajanakirao@gmail.com>
2026-09-18 12:37:51 +0530

    shell-script

--- 11-functions.sh
+++ 11-functions.sh
@@ -3,7 +3,7 @@
 
 USER_ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$($0 cut -d '.' -f1)
+SCRIPT_NAME=$(echo $0 | cut -d '.' -f1)
 LOG_FILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 VALIDATE () {
