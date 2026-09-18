9e20f1116ebeeaec4b850ed907bbf17af56f6959
janidevops <konajanakirao@gmail.com>
2026-09-18 23:12:20 +0530

    shell-script

--- 14-installations.sh
+++ 14-installations.sh
@@ -22,10 +22,10 @@ fi
 VALIDATE () {
     if ([ $1 -ne 0 ])
         then
-            echo -e "$2 & $1 is...$R failed $N"
+            echo -e "$2 is...$R failed $N"
             exit 1
     else
-        echo -e "$2 & $1 is installed...$G successfully $N"
+        echo -e "$2 is installed...$G successfully $N"
     fi
 }
 
