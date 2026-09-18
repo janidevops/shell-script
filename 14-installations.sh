bad33db35a8a89687eb6e2f32751ceb3c48c508f
janidevops <konajanakirao@gmail.com>
2026-09-18 21:34:48 +0530

    shell-script

---
+++ 14-installations.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+
+if ([ $USER_ID -ne 0 ])
+ then
+    echo -e "$Y Please run this script as the root user $Y"
+    exit 1 # manually exit the script with a non-zero exit code
+else 
+    echo -e "$Y you are super user $Y"
+fi
+
+echo "All packages : $@"
+
+ 
