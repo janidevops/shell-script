212effee139518b6c12b7e9561d202df60d65dd5
janidevops <konajanakirao@gmail.com>
2026-09-18 11:21:12 +0530

    shell-script

---
+++ 11-functions.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+USER_ID=$(id -u)
+
+VALIDATE () {
+
+
+}
+
+if ([ $USER_ID -ne 0 ])
+ then
+    echo "Please run this script as the root user"
+    exit 1 # manually exit the script with a non-zero exit code
+else 
+    echo "you are super user"
+fi
+
+dnf install mysql-server -y
+VALIDATE $? "Installation of mysql-server"
+
+dnf install git -y
+VALIDATE $? "Installation of git"
+
+
+
+echo "is script proceeding...?"
